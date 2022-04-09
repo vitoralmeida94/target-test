d7366cab1b3376e4a405b50c3b06cb8ba63a4057
vitoralmeida94 <vitorjurunas@gmail.com>
2022-04-09 00:09:13 -0300

    Projeto test

--- TargetTest.API/TargetTest.Core/Entities/Plano.cs
+++ TargetTest.API/TargetTest.Core/Entities/Plano.cs
@@ -7,15 +7,27 @@ namespace TargetTest.Core.Entities
 {
     public partial class Plano
     {
-        public Plano()
+        public Plano(int id, string nome, string descricao, decimal valor)
         {
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Valor = valor;
             Clientes = new HashSet<Cliente>();
         }
 
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public decimal Valor { get; set; }
+        public Plano(string nome, string descricao, decimal valor)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Valor = valor;
+            Clientes = new HashSet<Cliente>();
+        }
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Valor { get; private set; }
 
         public virtual ICollection<Cliente> Clientes { get; set; }
     }
---
+++ TargetTest.API/TargetTest.Tests/Repositories/PlanoRepositoryTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TargetTest.Core.Entities;
+using TargetTest.Infrastructure.Persistence.Repositories;
+using Xunit;
+
+namespace TargetTest.Tests.Repositories
+{
+    public class PlanoRepositoryTest
+    {
+        private PlanoRepository _repository;
+        public PlanoRepositoryTest()
+        {
+            Startup.Init();
+            _repository = new PlanoRepository(Startup._context);
+        }
+
+        [Theory]
+        [InlineData("PlanoTeste","Plano para testar",0.00)]
+        [InlineData("Plano VIP Testador", "Plano para testar", 150.00)]
+        [InlineData("Plano Test123", "Plano para testar", 1200.00)]
+        [InlineData("Plano Beta555", "Plano para testar", 888.00)]
+        public void InsertPlanoSuccess(string nome, string descricao, decimal valor)
+        {
+            var plano = new Plano(nome,descricao,valor);
+            var planoResultado = _repository.Insert(plano);
+            Assert.NotNull(planoResultado);
+            Assert.NotEqual(0, plano.Id);
+            Assert.Equal(plano.Descricao, planoResultado.Descricao);
+            Assert.Equal(plano.Valor, planoResultado.Valor);
+            Assert.Equal(plano.Nome, planoResultado.Nome);
+        }
+      
+
+    }
+}
